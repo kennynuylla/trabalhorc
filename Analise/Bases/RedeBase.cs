cdf726bf5e569b140618fa009b06acd5ec175510
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-23 14:05:31 -0300

    RedeBase refatorada

--- Analise/Bases/RedeBase.cs
+++ Analise/Bases/RedeBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Analise.Constantes;
 using Analise.Interfaces;
 using PySharp;
 
@@ -7,21 +8,17 @@ namespace Analise.Bases
 {
     public abstract class RedeBase : IRede
     {
-        private const string _pastaTodasRedes = "Redes/";
-        private const string _arquivoCriar = "criar.py";
-        private const string _diretórioTmp = "tmp/";
-        private const string _baseNomeListaArestas = "rede_arestas";
-        private const string _extensão = ".txt";
-
+        private readonly RedeBaseConstantes _constantes = new RedeBaseConstantes();
         private readonly string _caminhoPython;
         private readonly string _listaArestas;
 
-        public string DiretórioTmp { get => $"{_caminhoPython}{_pastaTodasRedes}{_caminhoRede}{_diretórioTmp}";}
-        public string ListaArestas { get => $"{DiretórioTmp}{_listaArestas}"; }
-        public string ScriptCriarRelativo { get => $"{_pastaTodasRedes}{_caminhoRede}{_arquivoCriar}"; }
-
         protected readonly Pysharp _pysharp;
         protected readonly string _caminhoRede;
+
+        public string DiretórioTmp { get => $"{_caminhoPython}{_constantes.PastaTodasRedes}{_caminhoRede}{_constantes.DiretórioTmp}";}
+        public string ListaArestas { get => $"{DiretórioTmp}{_listaArestas}"; }
+        public string ScriptCriarRelativo { get => $"{_constantes.PastaTodasRedes}{_caminhoRede}{_constantes.ArquivoCriar}"; }
+
 
         public RedeBase(string caminhoPython, string caminhoRede, string executávelPython)
         {
@@ -29,22 +26,11 @@ namespace Analise.Bases
             _caminhoRede = caminhoRede;
             _caminhoPython = caminhoPython;
 
-            var tmpListaArestas = _baseNomeListaArestas;
-            var aleatório = new Random();
-
-            if(!Directory.Exists(DiretórioTmp))
-            {
-                Directory.CreateDirectory(DiretórioTmp);
-            }
+            CriarDiretórioTemporário();
+            _listaArestas = CriarArquivoListaArestas();
             
-            while(File.Exists($"{DiretórioTmp}{tmpListaArestas}{_extensão}"))
-            {
-                tmpListaArestas = $"{tmpListaArestas}{aleatório.Next(10)}";
-            }
-
-            _listaArestas = $"{tmpListaArestas}{_extensão}";
-            File.Create(ListaArestas);
         }
+
 
         public virtual void CriarRede()
         {
@@ -60,6 +46,32 @@ namespace Analise.Bases
         public void Dispose()
         {
             File.Delete(ListaArestas);
+        }
+
+        private void CriarDiretórioTemporário()
+        {
+            if (!Directory.Exists(DiretórioTmp))
+            {
+                Directory.CreateDirectory(DiretórioTmp);
+            }
+        }
+
+        private string CriarArquivoListaArestas()
+        {
+            string retorno;
+
+            var tmpListaArestas = _constantes.BaseNomeListaArestas;
+            var aleatório = new Random();
+
+            while (File.Exists($"{DiretórioTmp}{tmpListaArestas}{_constantes.Extensão}"))
+            {
+                tmpListaArestas = $"{tmpListaArestas}{aleatório.Next(10)}";
+            }
+
+            retorno = $"{tmpListaArestas}{_constantes.Extensão}";
+            File.Create(retorno);
+
+            return retorno;
         }
     }
 }
