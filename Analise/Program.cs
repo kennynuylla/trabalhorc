388c517a72984fb5abdd7e04010e9a4b813019e9
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-22 23:18:16 -0300

    Início para refatoração para C#

---
+++ Analise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PySharp;
+
+namespace Analise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pysharp = new Pysharp("/codigo/py", "python3");
+        }
+    }
+}
