d274adb25dec5e30489f2f6cb795385ba3e41d1d
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-23 21:16:52 -0300

    A rede criada extrai os valores de importância e latência efetiva média

--- py/Redes/Debug/criar.py
+++ py/Redes/Debug/criar.py
@@ -1,7 +1,7 @@
 import sys, networkx as nx, random as rnd
 sys.path.append("/codigo/py/Comum")
 
-import netlist
+import netlist, netanalise
 
 rede = nx.fast_gnp_random_graph(5, 0.8)
 
@@ -10,9 +10,14 @@ arestas = rede.edges(data=True)
 
 for nó in nós:
     nó[1]["importância"] = rnd.randint(0,10)
+    nó[1]["importância_final"] = nó[1]["importância"] + rede.degree(nó[0])
+
 
 for aresta in arestas:
     aresta[2]["latência_efetiva"] = rnd.random()
 
 netlist.GerarEdgeList(sys.argv[1], rede)
 netlist.GerarNodeList(sys.argv[2], rede)
+
+i,l = netanalise.Analisar(rede)
+print("%s|%s" %(i,l))
