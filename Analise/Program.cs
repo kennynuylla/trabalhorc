e770ebcc84cfac0b2d0be0827f02b054fde93d2c
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-26 11:03:08 -0300

    Lista de nós não guarda mais o grau dos nós

--- Analise/Program.cs
+++ Analise/Program.cs
@@ -12,8 +12,11 @@ namespace Analise
             using(IRede rede = new DebugRede())
             {
                 rede.CriarRede();
+/*                IRede clone = rede.Clone() as DebugRede;
+                rede.PlotarRede("/codigo/png/original.png");
+                clone.PlotarRede("/codigo/png/clone.png");
                 Console.WriteLine("Rede Criada");
-
+*/
                 IAnalisador analisador = new AnalisadorBiblioteca();
                 analisador.AnalisarRede(rede);
                 Console.WriteLine("Rede Analisada");
--- py/Comum/netplot.py
+++ py/Comum/netplot.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt, networkx as nx
 def PlotarRede(rede, caminhoPlot):
     plt.figure(figsize=(12,12), dpi=128)
     labels = nx.get_node_attributes(rede, "importância")
-    edge_labels = nx.get_edge_attributes(rede, "latência_efetiva")
     nx.draw(rede, labels=labels, node_size=1000, font_size=20)
     plt.savefig(caminhoPlot)
     plt.close()
