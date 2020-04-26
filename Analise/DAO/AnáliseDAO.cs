6d7e3d6b193e38e5e898ecf075ee37a3c2d6c76d
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-26 16:57:51 -0300

    Resulado exportado para CSV

--- Analise/DAO/AnáliseDAO.cs
+++ Analise/DAO/AnáliseDAO.cs
@@ -3,15 +3,17 @@ namespace Analise.DAO
     public class AnáliseDAO
     {
 
-        public double[] ImportânciaTotal { get; set; }
-        public double[] LatênciaEfetivaMédia { get; set; }
-        public double[] TaxaRemoção { get; set; }
+        public double[] ImportânciaTotal { get; }
+        public double[] LatênciaEfetivaMédia { get; }
+        public double[] TaxaRemoção { get; }
+        public int QuantidadePontos { get; }
         
-        public AnáliseDAO(double[] importânciaTotal, double[] latênciaEfetivaMédia, double[] taxaRemoção)
+        public AnáliseDAO(double[] importânciaTotal, double[] latênciaEfetivaMédia, double[] taxaRemoção, int quantidadePontos)
         {
             ImportânciaTotal = importânciaTotal;
             LatênciaEfetivaMédia = latênciaEfetivaMédia;
             TaxaRemoção = taxaRemoção;
+            QuantidadePontos = quantidadePontos;
         }
     }
 }
--- Analise/DAO/ListaArquivosDAO.cs
+++ Analise/DAO/ListaArquivosDAO.cs
@@ -1,11 +1,11 @@
 namespace Analise.DAO
 {
-    public class ListasArquivosDAO
+    public class ListaArquivosDAO
     {
         public string ListaArestas { get; set; }
         public string ListaNós { get; set; }
         
-        public ListasArquivosDAO(string listaArestas, string listaNós)
+        public ListaArquivosDAO(string listaArestas, string listaNós)
         {
             ListaArestas = listaArestas;
             ListaNós = listaNós;
