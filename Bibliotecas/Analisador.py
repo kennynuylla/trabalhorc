efb764bc4bbbf1f47345d9e111347e3d0bf241aa
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-04-28 18:43:30 -0300

    Código refatorado e ataque adicionado

--- Bibliotecas/Analisador.py
+++ Bibliotecas/Analisador.py
@@ -1,4 +1,4 @@
-import numpy as np, matplotlib.pyplot as plt, networkx as nx, copy as cp
+import numpy as np, matplotlib.pyplot as plt, networkx as nx, copy as cp, sys
 
 
 def encontrar_importância_total(rede):
@@ -30,24 +30,50 @@ def remover_nós_falha(rede, probabilidade):
 
     rede.remove_nodes_from(para_deletar)
 
-def simular_falha(rede, probabilidade):
-    remover_nós_falha(rede, probabilidade)
+def encontrar_maior_cluster(componentes, rede):
+    importância = 0
+    maior_cluster = None
 
+    for componente in componentes:
+        importância_componente = encontrar_importância_componente(componente, rede)
+        if(importância_componente > importância):
+            importância = importância_componente
+            maior_cluster = componente
+
+    return maior_cluster, importância
+
+def analisar_componentes(rede):
     componentes_conectadas = nx.connected_components(rede)
 
     if(nx.number_connected_components(rede) == 0):
         return 0, 0
     
-    maior_cluster = max(componentes_conectadas, key=(lambda item: encontrar_importância_componente(item,rede)))
+    maior_cluster, importância = encontrar_maior_cluster(componentes_conectadas, rede)
     maior_cluster = rede.subgraph(maior_cluster).copy()
 
-    return encontrar_importância_total(maior_cluster), encontrar_latência_efetiva_média(maior_cluster)
+    return importância, encontrar_latência_efetiva_média(maior_cluster)
 
-def gerar_pontos_resiliência(rede, b,x):
+def simular_falha(rede, probabilidade):
+    remover_nós_falha(rede, probabilidade)
+    return analisar_componentes(rede)
+
+
+def simular_ataque(nós, rede, probabilidade):
+    para_remover = []
+    qtd_para_remover = int(np.round(probabilidade * len(nós)))
+    for i in range(qtd_para_remover):
+        para_remover.append(nós[i][0])
+
+    rede.remove_nodes_from(para_remover)
+    return analisar_componentes(rede)
+
+
+def gerar_pontos_falha(rede, b,x):
     probabilidades = np.linspace(0,1,x)
     importâncias = np.zeros(x)
     latências_efetivas = np.zeros(x)
 
+    print("Iniciando análise de falha")
 
     for n in range(b):
         for i in range(x):
@@ -67,6 +93,41 @@ def gerar_pontos_resiliência(rede, b,x):
     #Normalizar
     importâncias /= importâncias[0]
     latências_efetivas /= latências_efetivas[0]
+
+    print("Análise de falha finalizada")
+
+    return importâncias, latências_efetivas, probabilidades
+
+def gerar_pontos_ataque(rede, b, x):
+    probabilidades = np.linspace(0,1,x)
+    importâncias = np.zeros(x)
+    latências_efetivas = np.zeros(x)
+
+    print("Iniciando análise de ataque")
+
+    nós = list(rede.nodes(data=True))
+    nós.sort(reverse=True, key = lambda item: item[1]["importância"] + rede.degree(item[0]))
+
+    for n in range(b):
+        for i in range(x):
+            print("Começando probabilidade de remoção de %.2f (%d/%d)" %(probabilidades[i], i+1, x))
+            clone = cp.deepcopy(rede)
+            importância, latência_efetiva = simular_ataque(nós, clone, probabilidades[i])
+            importâncias[i] += importância
+            latências_efetivas[i] += latência_efetiva
+            print("Finalizado probabilidade de remoção de %.2f (%d/%d)" %(probabilidades[i], i+1, x))
+
+        print("Finalizando %d/%d" %(n+1, b))
+
+    #Calcular Média
+    importâncias /= b
+    latências_efetivas /= b
+
+    #Normalizar
+    importâncias /= importâncias[0]
+    latências_efetivas /= latências_efetivas[0]
+
+    print("Análise de ataque finalizaada")
 
     return importâncias, latências_efetivas, probabilidades
 
